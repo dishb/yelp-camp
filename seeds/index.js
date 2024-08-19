c17b1f6de768082e97c856ec410ed1d413dc960d
dishb <code.dishb@gmail.com>
2024-08-18 19:34:08 -0700

    feat: added C functionality of CRUD

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const cities = require('./cities)');
+const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 
 const Campground = require('../models/campground');
